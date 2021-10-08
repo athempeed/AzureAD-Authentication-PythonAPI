3337be4f61522fd46024248722ad0b697302d9f3
donjay2101 <mehta.deepak@outlook.com>
2021-10-08 19:35:04 +0800

    updated appConfig.py

--- appConfig.py
+++ appConfig.py
@@ -1,4 +1,4 @@
 
-CLIENT_ID = "307d4626-d62f-45b3-bddb-24db6cb19c81" # Application (client) ID of app registration
-CLIENT_SECRET = "TH.7Q~YTHvea_oee.K6TixoFAs7b0cLKdUGwt"
-TENANT_ID="8b24551d-7c2c-4beb-8b61-95f32d9929ef"
+CLIENT_ID = "" # Application (client) ID of app registration
+CLIENT_SECRET = ""
+TENANT_ID=""
